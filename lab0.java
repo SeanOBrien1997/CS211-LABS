203eb39cf7eb56f97f743cff54a72d0fd9294bd5
Sean O Brien <43688664+SeanOBrien1997@users.noreply.github.com>
2019-02-13 00:02:06 +0000

    Add files via upload

--- lab0.java
+++ lab0.java
@@ -1,29 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+/*
+ * This is steaming trash
+ */
 public class lab0 {
-
-	public static String sha256(String input) throws NoSuchAlgorithmException {
-		byte[] in = hexStringToByteArray(input);
-		MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
-		byte[] result = mDigest.digest(in);
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < result.length; i++) {
-			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-		}
-		return sb.toString();
-	}
-
-	public static byte[] hexStringToByteArray(String s) {
-		int len = s.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) {
-			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
-		}
-		return data;
-	}
 
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		SecureRandom rand = new SecureRandom(); // better than regular random
@@ -51,48 +40,94 @@ public class lab0 {
 		System.out.println("Double Eighty + 8 Hash: " + doubleeighty);
 
 		// compute 58
+		String secret = base58(base10(doubleeighty));
+		System.out.println("Your Bit/Vertcoin private key is: " + secret);
+		
+		// optional
+		System.out.println("Output to textfile? (Y/n): ");
+		Scanner sc = new Scanner(System.in);
+		String reply = sc.nextLine().toUpperCase();
+		if(reply.equals("Y")) {
+			try {
+				PrintStream out = new PrintStream(new FileOutputStream("secret.txt"));
+				System.setOut(out);
+				System.out.println(secret);
+			} catch (FileNotFoundException e) {
+				System.out.println("Error outputing key to text file");
+				e.printStackTrace();
+			}
+		}
 	}
 
-/*	public static String base58(String s) {
-
+	public static String sha256(String input) throws NoSuchAlgorithmException {
+		byte[] in = hexStringToByteArray(input);
+		MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+		byte[] result = mDigest.digest(in);
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < result.length; i++) {
+			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
 	}
-*/
+
+	public static byte[] hexStringToByteArray(String s) {
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+		}
+		return data;
+	}
+	
+	public static String base58(BigInteger bi) {
+		List<BigInteger> list = new ArrayList<BigInteger>();
+		while(!bi.equals(BigInteger.ZERO)) {
+			list.add(bi.mod(BigInteger.valueOf(58)));
+			bi = bi.divide(BigInteger.valueOf(58));
+		}
+		String[] alphabet = {"1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+		String result = "";
+		for(int i = list.size()-1;i>=0;i--) {
+			result += alphabet[(int) list.get(i).longValue()];
+		}
+		return result;
+	}
+
 	public static BigInteger base10(String s) {
 		BigInteger bi = BigInteger.valueOf(0);
 		int count = 0;
 		for (int i = s.length() - 1; i >= 0; i--) {
-			long total = 0;
+			BigInteger total = BigInteger.valueOf(16);
 			String currentNum = "" + s.charAt(i);
-			System.out.println(currentNum);
 			try {
 				long num = Long.parseLong(currentNum);
-				total = (long) (Math.pow(16, count) * num);
-				bi.add(BigInteger.valueOf(total));
+				total = (total.pow(count).multiply(BigInteger.valueOf(num)));
+				bi = bi.add(total);
 			} catch (Exception e) {
 				switch (currentNum.toUpperCase()) {
 				case "A":
-					total = (long) (Math.pow(16, count) * 10);
-					bi.add(BigInteger.valueOf(total));
+					total = (total.pow(count).multiply(BigInteger.valueOf(10)));
+					bi = bi.add(total);
 					break;
 				case "B":
-					total = (long) (Math.pow(16, count) * 11);
-					bi.add(BigInteger.valueOf(total));
+					total = (total.pow(count).multiply(BigInteger.valueOf(11)));
+					bi = bi.add(total);
 					break;
 				case "C":
-					total = (long) (Math.pow(16, count) * 12);
-					bi.add(BigInteger.valueOf(total));
+					total = (total.pow(count).multiply(BigInteger.valueOf(12)));
+					bi = bi.add(total);
 					break;
 				case "D":
-					total = (long) (Math.pow(16, count) * 13);
-					bi.add(BigInteger.valueOf(total));
+					total = (total.pow(count).multiply(BigInteger.valueOf(13)));
+					bi = bi.add(total);
 					break;
 				case "E":
-					total = (long) (Math.pow(16, count) * 14);
-					bi.add(BigInteger.valueOf(total));
+					total = (total.pow(count).multiply(BigInteger.valueOf(14)));
+					bi = bi.add(total);
 					break;
 				case "F":
-					total = (long) (Math.pow(16, count) * 15);
-					bi.add(BigInteger.valueOf(total));
+					total = (total.pow(count).multiply(BigInteger.valueOf(15)));
+					bi = bi.add(total);
 					break;
 				}			
 			}
