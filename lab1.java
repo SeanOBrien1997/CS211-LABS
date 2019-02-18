4bbc930da3f7edd089cfb8950688d4b7a0966bd1
Sean O Brien <43688664+SeanOBrien1997@users.noreply.github.com>
2019-02-18 11:23:23 +0000

    Add files via upload

--- lab1.java
+++ lab1.java
@@ -7,17 +7,30 @@ public class lab1 {
 		HashMap<String, Counter> letterMap = new HashMap<String, Counter>();
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
+		String printBin = "";
 		for(int i = 0;i<s.length();i++){
 			String current = "";
 			current += s.charAt(i);
 			String binary = Integer.toBinaryString((int)current.charAt(0));
+			printBin += pad(binary + " ");
 			if(letterMap.containsKey(binary)){
 				letterMap.get(binary).increase();
 			} else {
 				letterMap.put(binary, new Counter());
 			}
 		}
+		System.out.println(printBin);
 		letterMap.forEach((key,value) -> System.out.println("'" + (char)Integer.parseInt(key, 2) + "'" + " appeared " + value.get() + " times"));
+	}
+	
+	public static String pad(String s){
+		int padding = 8-s.length();
+		String result = "";
+		for(int i = 0; i < padding; i++){
+			result += "0";
+		}
+		result += s;
+		return result;
 	}
 }
 
